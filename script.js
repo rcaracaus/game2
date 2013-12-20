4288c8274669956681a9c5378c45dfc21ff4534c
elijahdisch <elijahdisch@hotmail.com>
2013-12-21 00:17:48 +1100

    got ball working with variables

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-
-var x = 0;
-var y = 0;
-var w = 50;
-
+var c = document.getElementById("pane");
 var width = c.width;
 var height = c.height;
 
-var cmTID;
+var ctx = c.getContext("2d");
+
+var ball = new Object();
+
+ball.w = 50;
+ball.x = 0;
+ball.y = 0;
+ball.vy = 1;
+ball.vx = 1;
+
+var px, py, cmTID;
+
+document.addEventListener('click', onMouseClick, false);
+
+function onMouseClick(event) {
+	px = event.pageX;
+	py = event.pageY;
+}
+
+function reset() {
+	ball.x = 0;
+	ball.y = 0;
+	ball.vx = 1;
+	ball.vy = 1;
+	animate();
+}
 
 function animate() {
 
-	if (x + w < width && y + w < height) {
+	if (ball.x + ball.w < width && ball.y + ball.w < height) {
 
 		ctx.clearRect(0, 0, width, height);
-		ctx.fillStyle = "#FF0000";
-		ctx.strokeRect(x, y, w, w);
+		ctx.strokeStyle = "#FF0000";
+		ctx.strokeRect(ball.x, ball.y, ball.w, ball.w);
 
-		x++;
-		y++;
+		ball.x += ball.vx;
+		ball.y += ball.vy;
 
 		clearTimeout(cmTID);
 		cmTID = setTimeout(animate, 50);
-		
+
 	}
+
+	var mouseX = px - c.getBoundingClientRect().left;
+	var mouseY = py - c.getBoundingClientRect().top;
+
+	if ((mouseX >= ball.x && mouseX <= ball.x + ball.w) && (mouseY >= ball.y && mouseY <= ball.y + ball.w)) {
+		ball.vy += 0.5;
+	}
+
+	console.log("x: " + mouseX + " y: " + mouseY);
 
 }
 
-animate();
