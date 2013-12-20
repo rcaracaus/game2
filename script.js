3b82b9f8fcf06d70de548adebcdfdf126713b88a
elijahdisch <elijahdisch@hotmail.com>
2013-12-20 20:34:17 +1100

    added animation inside second canvas

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("here");
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var x = 0;
+var y = 0;
+var w = 50;
+
+var width = c.width;
+var height = c.height;
+
+var cmTID;
+
+function animate() {
+
+	if (x + w < width && y + w < height) {
+
+		ctx.clearRect(0, 0, width, height);
+		ctx.fillStyle = "#FF0000";
+		ctx.strokeRect(x, y, w, w);
+
+		x++;
+		y++;
+
+		clearTimeout(cmTID);
+		cmTID = setTimeout(animate, 50);
+		
+	}
+
+}
+
+animate();
