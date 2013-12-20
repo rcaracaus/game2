2ad7aac9161d63f5f30a7b4631c4c6ac07171e6d
elijahdisch <elijahdisch@hotmail.com>
2013-12-21 02:19:32 +1100

    alpha v0.1

--- script.js
+++ script.js
@@ -6,53 +6,65 @@ var ctx = c.getContext("2d");
 
 var ball = new Object();
 
-ball.w = 50;
-ball.x = 0;
-ball.y = 0;
-ball.vy = 1;
-ball.vx = 1;
 
-var px, py, cmTID;
 
-document.addEventListener('click', onMouseClick, false);
+var mouseX, mouseY, cmTID;
 
-function onMouseClick(event) {
-	px = event.pageX;
-	py = event.pageY;
+function cursorInBall() {
+	if ((mouseX >= ball.x && mouseX <= ball.x + ball.w) && (mouseY >= ball.y && mouseY <= ball.y + ball.w)) {
+		return true;
+	} else { 
+		return false; 
+	}
 }
 
-function reset() {
+function animate() {
+	if (ball.x + ball.w < width && ball.y + ball.w < height) {
+		ball.x += ball.vx;
+		ball.y += ball.vy;
+	}
+	ctx.clearRect(0, 0, width, height);
+	ctx.strokeStyle = "#FF0000";
+	ctx.strokeRect(ball.x, ball.y, ball.w, ball.w);
+	clearTimeout(cmTID);
+	cmTID = setTimeout(animate, 50);
+}
+
+
+function onMouseMove(event) {
+	
+}
+
+
+function start() {
 	ball.x = 0;
 	ball.y = 0;
 	ball.vx = 1;
 	ball.vy = 1;
+	ball.w = 50;
+	ball.click = false;
 	animate();
 }
 
-function animate() {
 
-	if (ball.x + ball.w < width && ball.y + ball.w < height) {
-
-		ctx.clearRect(0, 0, width, height);
-		ctx.strokeStyle = "#FF0000";
-		ctx.strokeRect(ball.x, ball.y, ball.w, ball.w);
-
-		ball.x += ball.vx;
-		ball.y += ball.vy;
-
-		clearTimeout(cmTID);
-		cmTID = setTimeout(animate, 50);
-
+$(document).mousemove(function(event) {
+    mouseX = event.pageX - c.getBoundingClientRect().left;
+    mouseY = event.pageY - c.getBoundingClientRect().top;
+    console.log(ball.x + '-' + mouseX);
+    if (cursorInBall()) {
+	  $('canvas').css('cursor', 'pointer');
+	} else {
+	  $('canvas').css('cursor', 'auto');
 	}
+ });
 
-	var mouseX = px - c.getBoundingClientRect().left;
-	var mouseY = py - c.getBoundingClientRect().top;
+$(document).ready(function () {
 
-	if ((mouseX >= ball.x && mouseX <= ball.x + ball.w) && (mouseY >= ball.y && mouseY <= ball.y + ball.w)) {
-		ball.vy += 0.5;
-	}
+	$('canvas').click( function() {
+		if (cursorInBall()) {
+			ball.vy += 0.5;
+		}
+	});
 
-	console.log("x: " + mouseX + " y: " + mouseY);
-
-}
+});
 
