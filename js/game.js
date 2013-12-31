167adf8f93cf14e8789c829ebc4e5498560b56d3
Robert Caracaus <rcaracaus@gmail.com>
2013-12-31 11:20:15 +1100

    Made all dom elements use same object for creation and settings

--- js/game.js
+++ js/game.js
@@ -1,48 +1,41 @@
 /* Global Vars */
 var refreshIntervalId;
 
-targets = 10;
-targetArray = new Array();
-players = 2;
-playerArray = new Array();
+domElements = {
+    targets: {
+        name: "target",
+        amount: 10,
+        items: new Array()
+    },
+    players: {
+        name: "player",
+        amount: 2,
+        items: new Array()
+    }
+}
+
 fps = 60;
-playersCreated = 0;
-resetRan = false;
 window.points = 0;
 
+
 function init() {
-
-    startTimer();
-
-    // Better performance to set variables once?
     windowHeight = $(window).height();
     windowWidth = $(window).width();
     w = windowWidth / 30;
 
+    startTimer();
     clearInterval(refreshIntervalId);
-    createElements(targets, "target");
-    createElements(players, "player");
+
+    createElements(domElements);
 
     // Target's position should reset on init.
     reset();
-    setKeys();
-    
+    setKeys(domElements);
     // Start animation
-
-    animate( function() {
-
-        targetArray.forEach( function(element, i) {
-            animateFallTargets(element, i);
-            // Prompt each target to fall separately.
-            elementMove("target", i,  element.x, element.y);
-        });
-
-        playerArray.forEach( function(element, i) {
-            elementMove("player", i,  element.x, element.y);
-        });
-
+    animate(function() {
+        animateTargets(domElements);
+        elementMove(domElements);
         detectCollision();
-
     });
 }
 
@@ -50,117 +43,100 @@ function reset() {
     setLocalHighScore();
     resetScore();
     startTimer();
-    resetTargetPosition(targets);
-    resetPlayerPosition(players);
 }
 
-function createElements(amount, name) {
-	for (var i = 0; i < amount; i++) {
-		div = document.createElement( 'div' );
-        div.id = name + '-' + i;
-		div.className = name + ' poo1' + ' ' + div.id + " " + div.rowClass;
-        div.style.width = w + "px";
-        div.style.height = w + "px";
-        div.style.top = -w + "px"; // Positions element offscreen initially
-		document.body.appendChild(div);
-	}
-}
-
-function elementMove(name, i, x, y) {
-    var elem = document.getElementById(name + '-' + i);
-    elem.style.size = '100px';
-    elem.style.top = y + "px";
-    elem.style.left = x + "px";
-}
-
-function resetTargetPosition(amount) {
-    // Reset each target's position on each init.
-    for (var i = 0; i < amount; i++) {
-        targetArray[i] = {
-            id: i,
-            x: Math.floor(Math.random() * (windowWidth - w)),
-            y: 0 - w,
-            width: w,
-            height: w,
-            vy: Math.random() + 0.5,
-            isAlive: true,
-            lowestY: false
-        }
-    }
-
-
-     var elems = document.getElementsByClassName('target');
-     for(var i = 0; i < elems.length; i++) {
-        elems[i].style.display = "block";
-     }
-
-}
-
-function resetPlayerPosition(amount) {
-    // Reset each player's position on each init.
-    for (var i = 0; i < amount; i++) {
-        playerArray[i] = {
-            "x": i *100,
-            "y": windowHeight - w,
-            "width": w,
-            "height": w,
-            "selected": true
+function createElements(object) {
+    for(var index in object) {
+        for (var i = 0; i < object[index].amount; i++) {
+            object[index].items[i] = document.createElement('div');
+            object[index].items[i].id = object[index].name + '-' + i;
+            object[index].items[i].x = Math.floor(Math.random() * (windowWidth - w));
+            object[index].items[i].y = 0 - w;
+            object[index].items[i].width = w;
+            object[index].items[i].height = w;
+            object[index].items[i].vy = Math.random() + 0.5;
+            object[index].items[i].isAlive = true;
+            object[index].items[i].lowestY = false;
+            object[index].items[i].selected = true;
+            object[index].items[i].className = object[index].name + ' poo1' + ' ';
+            object[index].items[i].style.width = w + "px";
+            object[index].items[i].style.height = w + "px";
+            object[index].items[i].style.top = -w + "px"; // Positions element offscreen initially
+            document.body.appendChild(object[index].items[i]);
         }
     }
 }
 
-function animateFallTargets(element, i) {
-
-    var elems = document.getElementsByClassName('target');
-
-    if (isInWindow(element)) {
-        element.y += element.vy;
-    } else {
-        elems[i].style.display = "block";
-        element.isAlive = true;
-        element.x = Math.floor(Math.random() * (windowWidth - w));
-        element.y = 0 - w;
+function elementMove(object) {
+    for(var index in object) {
+        object[index].items.forEach( function(element, i) {
+            var elem = document.getElementById(object[index].name + '-' + i);
+            elem.style.size = '100px';
+            elem.style.top = element.y + "px";
+            elem.style.left = element.x + "px";
+        });
     }
-
 }
 
-function setKeys(element, i) {
 
+function animateTargets(domElements) {
+    domElements.targets.items.forEach( function(element, i) {
+        var elems = document.getElementsByClassName('target');
+
+        if (isInWindow(element)) {
+            element.y += element.vy;
+        } else {
+            elems[i].style.display = "block";
+            element.isAlive = true;
+            element.x = Math.floor(Math.random() * (windowWidth - w));
+            element.y = 0 - w;
+        }
+    });
+}
+
+function setKeys(object) {
+
+    object.players.items.forEach( function(element, i) {
+        element.y = windowHeight - w;
+    });
+
+    // Right Key
     $.fastKey('39', function() {
-        playerArray.filter(function(item) {
+        console.log('left');
+        object.players.items.filter(function(item) {
             return item.selected;
         })[0].x += 2;
     });
 
+    // Left Key
     $.fastKey('37', function() {
-        playerArray.filter(function(item) {
+        object.players.items.filter(function(item) {
             return item.selected;
         })[0].x -= 2;
     });
 
-    selectedPlayer();
+    selectedPlayer(object);
 
 }
 
-function selectedPlayer() {
-
+function selectedPlayer(object) {
     var selected;
-    playerArray[1].selected = false;
-    $('.player-0').addClass('active');
+    object.players.items[1].selected = false;
+    $('#player-0').addClass('active');
 
     document.body.onkeydown = function(event){
         event = event || window.event;
         var keycode = event.charCode || event.keyCode;
         if(keycode === 9){
             event.preventDefault();
-            playerArray[0].selected = !playerArray[0].selected;
-            playerArray[1].selected = !playerArray[1].selected;
+            object.players.items[0].selected = !object.players.items[0].selected;
+            object.players.items[1].selected = !object.players.items[1].selected;
 
-            loop(playerArray, function(i) {
-                if (playerArray[i].selected) {
-                    $('.player-' + i).toggleClass('active');
+            loop(object.players.items, function(i) {
+                if (object.players.items[i].selected) {
+                    $('#player-' + i).toggleClass('active');
                 } else {
-                    $('.player-' + i).removeClass('active');
+                    $('#player-' + i).removeClass('active');
                 }
             });
 
@@ -180,14 +156,13 @@ function isInWindow(element) {
 }
 
 function detectCollision() {
-    targetArray.forEach(function(element) {
-        playerArray.forEach(function(player) {
+    domElements.targets.items.forEach(function(element) {
+        domElements.players.items.forEach(function(player) {
             if(intersects(player, element) && element.isAlive) {
                 window.points++;
                 document.getElementById("points").innerHTML = window.points;
                 element.isAlive = false;
-
-                var elem = document.getElementById('target-' + element.id);
+                var elem = document.getElementById(element.id);
                 elem.style.display = "none";
             }
         });
@@ -207,7 +182,6 @@ function startTimer() {
             startTimer();
             return;
         }
-
         $(".timer").text(count);
     }
 }
